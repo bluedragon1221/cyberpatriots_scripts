41100946179fa7697b5e4567f0eec70e57687582
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-11-05 08:17:14 -0600

    split into files

---
+++ lib.lua
@@ -0,0 +1,49 @@
+LOG_LEVEL = "comment" -- "comment", "command", or "none"
+
+local M = {}
+
+function M.read_readme()
+  local file = io.open(os.getenv("HOME").."/readme.aspx")
+  if file then
+    local contents = file:read("*a")
+    file:close()
+    return contents
+  else
+    error("Can't find readme file at ~/readme.aspx")
+  end
+end
+
+function M.contains()
+  for _, i in ipairs(list) do
+    if value == i then
+      return true
+    end
+  end
+  return false
+end
+
+function write_line_log(command, msg)
+  local file = io.open(os.getenv("HOME").."/cp_log.txt", "a")
+  if file then
+    if msg then
+      file:write(command.." # "..msg.."\n")
+    else
+      file:write(command.."\n")
+    end
+    file:close()
+  else
+    error("Failed to open log file")
+  end
+end
+
+function M.log(command, msg)
+  write_line_log(command, msg)
+  
+  if LOG_LEVEL == "command" then
+    print(command)
+  elseif LOG_LEVEL == "comment" then
+    print(command.." # "..msg.."\n")
+  end
+end
+
+return M
---
+++ users.lua
@@ -0,0 +1,113 @@
+SUDO_GROUP = "wheel"
+SECURE_PASSWORD = "CP_2025!"
+
+local lib = require("lib")
+local contains = lib.contains
+
+function get_cur_users()
+  local file = io.open("/etc/passwd", "r")
+
+  local users = {}
+  while true do
+    local line = file:read("*l")
+    if line then
+      local pieces = {}
+      for item in string.gmatch(line, "[^:]+") do
+        table.insert(pieces, item)
+      end
+
+      if tonumber(pieces[3]) >= 1000 and pieces[1] ~= "root" then
+        table.insert(users, pieces[1])
+      end
+    else break end
+  end
+
+  return users
+end
+
+function get_cur_admins()
+  local file = io.open("/etc/group", "r")
+  local out = file:read("*a")
+  local wheel_line = out:match("\n("..SUDO_GROUP..":[^\n]*)\n")
+  if wheel_line then
+    local pieces = {}
+    for item in wheel_line:gmatch("[^:]+") do
+      table.insert(pieces, item)
+    end
+
+    local admins = {}
+    for admin in pieces[4]:gmatch("[^,]+") do
+      table.insert(admins, admin)
+    end
+
+    return admins
+  else
+    error("Failed to read wheel line. Incorrect SUDO_GROUP?")
+  end
+end
+
+function parse_user_data()
+  local html_content = lib.read_readme()
+  local pre_content = html_content:match("<h2>Authorized Administrators and Users</h2>[%s%S]-<pre>([%s%S]-)</pre>")
+  local admin_block, user_block = pre_content:match("<b>Authorized Administrators:</b>(.*)<b>Authorized Users:</b>(.*)")
+
+  local results = {
+    admins = {},
+    not_admins = {},
+    users = {},
+  }
+  
+  for username, password in admin_block:gmatch("%s*([%w_]+)[^\n]*\n%s*password:%s*([^\n]*)\n") do
+    table.insert(results.admins, username)
+    table.insert(results.users, username)
+  end
+  
+  for username in user_block:gmatch("(%w+)") do
+    table.insert(results.not_admins, username)
+    table.insert(results.users, username)
+  end
+
+  return results
+end
+
+local M = {}
+
+function M.debug_users_readme()
+  local user_data = parse_user_data()
+
+  print("Admins: ")
+  for _, admin in ipairs(user_data.admins) do
+    print("  "..admin)
+  end
+
+  print("Users: ")
+  for _, user in ipairs(user_data.not_admins) do
+    print("  "..user)
+  end
+end
+
+function M.check_users()
+  local users = get_cur_users()
+  local admins = get_cur_admins()  
+  local user_data = parse_user_data()
+
+  for _, user in ipairs(users) do
+    if contains(admins, user) and not contains(user_data.admins, user) then
+      M.log("gpasswd -d "..user.." "..SUDO_GROUP, "This user should NOT be an admin: "..user)
+    end
+
+    if not contains(admins, user) and contains(user_data.admins, user) then
+      M.log("usermod -aG "..SUDO_GROUP.." "..user, "This user SHOULD be an admin: "..user)
+    end
+
+    if not contains(user_data.users, user) then
+      M.log("deluser --remove-home "..user, "This user should not exist: "..user)
+    end
+
+    if contains(users, user) and contains(user_data.admins, user) then
+      M.log("printf "..user..":"..SECURE_PASSWORD.." | chpasswd", "Give this user a secure password: "..user)
+    end
+  end
+end
+
+return M
