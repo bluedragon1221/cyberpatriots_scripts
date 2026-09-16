55469f8a873bdfc203ae3aeb8e6d692331e914f6
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2026-09-16 12:53:57 -0500

    new privilege escalation flow

--- escalate.lua
+++ escalate.lua
@@ -9,16 +9,7 @@ end
 
 function M.escalate_privileges() 
   if get_uid() ~= 0 then
-    print("[!] Not running as root. Escalating privileges...")
-  
-    local script_path = arg[0]
-    local status = os.execute("sudo lua " .. script_path)
-  
-    if status ~= 0 then
-      os.execute("pkexec lua " .. script_path)
-    end
-  
-    os.exit(0)
+    print("[!] Not running as root. Rerun with sudo")
   end
 end
 
